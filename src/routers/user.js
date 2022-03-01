3c4dd3fd0900dd49f1c7861f4151645adbcd68c3
Abhay2712 <abhaysharma754@gmail.com>
2022-03-02 02:42:13 +0530

    Fixing bugs

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const multer = require('multer')
-const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
